acb776f6d1e1421d8e08ed603a3f089433a68c56
Jhielou Marie <jmsmamaradlo22@src.pshs.edu.ph>
2025-02-14 16:33:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function calculateCreatures(){
+	   
+     let initialPopulation = parseFloat(prompt('Enter the initial population :'));
+     let growthRate = parseFloat(prompt('Enter the rate of growth: '));
+	 let time = parseFloat(prompt('Enter the time in hours: '));
+	               
+     let finalPopulation = Math.round(initialPopulation*Math.pow(Math.E, (rateGrowth*time)));
+                      
+     let creaturesLocation = prompt('Enter the region or location of the monster: ');
+     let creaturesName = prompt('Enter the name of the monster: ');
+	 
+	 let creaturesInfo = creaturesLocation.concat(" ", creaturesName).toUpperCase();
+                      
+      document.getElementById("result").innerHTML = "After " + time + " hours, the population of " + creaturesInfo + " has increased to " + finalPopulation;
+      
+   }
